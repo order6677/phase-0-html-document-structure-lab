a22441fde16adddf8e1f05f1612db405c3386738
awadh <awadh.hassan@student.moringaschool.com>
2022-08-03 21:04:46 +0300

    update coomplete code

---
+++ function favoriteNumber.js
@@ -0,0 +1,8 @@
+function favoriteNumber(_num){
+    while (favoriteNumber < 10){
+        favoriteNumber +=10
+    }
+    return favoriteNumber;
+}
+favoriteNumber(0);
+console.log(favoriteNumber)
---
+++ samples.js
@@ -0,0 +1,99 @@
+// const age=14;
+
+// let isAdult;
+
+// if (age >=18){
+//     isAdult = true ;
+// } else{
+//     isAdult = false;
+
+// }
+
+// console.log(isAdult)
+
+
+// //  using true  or false//
+
+
+// const age=30;
+
+// let isAdult;
+
+// if (age >=18) 
+// {
+//     isAdult=true
+// }
+//    console.log(isAdult)
+
+
+
+
+// const name = "Alice";
+// let greeting;
+
+// if (name === "Alice") {
+//   greeting = "Hello, Alice!"
+// } else if (name === "The White Rabbit") {
+//   greeting = "Don't be late, White Rabbit"
+// } else if (name === "The Mad Hatter") {
+//   greeting = "Welcome to the tea party, Mad Hatter"
+// } else if (name === "The Queen of Hearts") {
+//   greeting = "Please don't chop off my head!"
+// } else {
+//   greeting = "Whoooo are you?"
+// }
+
+// console.log(greeting)
+// const age = 20;
+// let isAdult, canWork, canEnlist, canDrink;
+
+// if (age >= 21) {
+//   isAdult = true;
+//   canWork = true;
+//   canEnlist = true;
+//   canDrink = true;
+// } else if (age >= 18) {
+//   isAdult = true;
+//   canWork = true;
+//   canEnlist = true;
+// } else if (age >= 16) {
+//   canWork = true;
+// }
+// // => true
+
+// isAdult;
+// // => true
+
+// canWork;
+// // => true
+
+// canEnlist;
+// // => true
+
+// canDrink;
+// // => undefined
+
+
+
+
+
+
+
+// const age = 18;
+
+// let isAdult=false, canWork=false, canEnlist=false, canDrink=false;
+
+// if (age >= 21) {
+//   canWork = true;
+//   canEnlist = true;
+//   isAdult = true;
+//   canDrink = true;
+// } else if (age > 18) {
+//   canWork = true;
+//   canEnlist = true;
+//   isAdult = true;
+// } else if (age >= 16) {
+//   canWork = true;
+// }
+
+// console.log(`Age: ${age}\nCan work: ${canWork}\nCan enlist: ${canEnlist}\nIs a legal adult: ${isAdult}\nCan drink: ${canDrink}`);
